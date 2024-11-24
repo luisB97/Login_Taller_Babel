11ffec2175d64232eaa12ba9c7ad5f55bed7e4a3
Luis <Luis@DESKTOP-NUMT10T>
2024-11-24 16:12:04 -0600

    Agregar archivos de proyecto.

---
+++ Login_Taller_Babel/LoginTest.cs
@@ -0,0 +1,25 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Login_Taller_Babel
+{
+    public class Tests
+    {
+
+        [Test]
+        public void IngresoCorrecto()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.CssSelector("#login > button")).Click();
+
+            driver.Close();//Cerrar el navegador
+            driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
+
+        }
+    }
+}
