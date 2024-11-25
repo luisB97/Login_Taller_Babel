b44b06e0ea65b1c7afc85187d1d6709221ec53f8
Luis <luisgerardobarqueroaguilar@gmail.com>
2024-11-25 17:31:49 -0600

    Se agrego un nuevo test

--- Login_Taller_Babel/LoginTest.cs
+++ Login_Taller_Babel/LoginTest.cs
@@ -21,5 +21,21 @@ namespace Login_Taller_Babel
             driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
 
         }
+
+        [Test]
+        public void IngresoIncorrecto()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+
+            driver.FindElement(By.Id("username")).SendKeys("tomsmith1");
+            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
+            driver.FindElement(By.CssSelector("#login > button")).Click();
+
+            driver.Close();//Cerrar el navegador
+            driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
+
+        }
     }
 }
