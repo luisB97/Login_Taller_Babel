1ecaaa3c180a3ba41f51c8a09cd2ec15c43e4b67
Luis <luisgerardobarqueroaguilar@gmail.com>
2024-11-25 18:43:21 -0600

    Se agrega hooks

--- Login_Taller_Babel/Test/LoginTest.cs
+++ Login_Taller_Babel/Test/LoginTest.cs
@@ -6,37 +6,40 @@ namespace Login_Taller_Babel.Test
 {
     public class Tests
     {
+        //variables globales para que la vean todos los métodos de esta clase
+        public IWebDriver driver;
+        public LoginPage login;
+        public String baseURL = "https://the-internet.herokuapp.com/login";
+
+        //Los hooks ejecutan ciertos trozos de código en ciertas partes de mi test, con esto no tengo duplicidad de código y reutilizo código
+        [SetUp] //Ejecuta el trozo de código que esta en el SetUp y lo ejecuta antes de cada prueba
+        public void IniciarNavegador() //void porque no retorna ningún parámetro
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
+            driver.Navigate().GoToUrl(baseURL);
+
+            login = new LoginPage(driver); //El driver definido en el test va a viajar a LoginPage
+        }
+
+        [TearDown]
+        public void CerrarNavegador() 
+        {
+            driver.Close();//Cerrar el navegador
+            driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
+        }
 
         [Test]
         public void IngresoCorrecto()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-
-            LoginPage login = new LoginPage(driver); //El driver definido en el test va a viajar a LoginPage
-
             login.IngresarCredenciales();
-
-            driver.Close();//Cerrar el navegador
-            driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
 
         }
 
         [Test]
         public void IngresoIncorrecto()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
-            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith1");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
-            driver.FindElement(By.CssSelector("#login > button")).Click();
-
-            driver.Close();//Cerrar el navegador
-            driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
-
+            login.IngresarCredenciales();
         }
     }
 }
