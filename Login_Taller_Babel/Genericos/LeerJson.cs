c2ff2189576c701465f970c20afe65cd08fdfcfc
Luis <luisgerardobarqueroaguilar@gmail.com>
2024-11-25 23:23:24 -0600

    Se agrega TestSource

--- Login_Taller_Babel/Genericos/LeerJson.cs
+++ Login_Taller_Babel/Genericos/LeerJson.cs
@@ -9,10 +9,11 @@ namespace Login_Taller_Babel.Genericos
 {
     public class LeerJson
     {
-        public POCO.LoginData login_data()
+        public List<POCO.LoginData> login_data()
         {
-            var json = JsonConvert.DeserializeObject<POCO.LoginData>(File.ReadAllText(@"..\..\..\Data\login.json"));
-            return json;
+            var json = JsonConvert.DeserializeObject<Dictionary<String,List<POCO.LoginData>>>(File.ReadAllText(@"..\..\..\Data\login.json"));
+            //return json; //Sino es un diccionario
+            return json["credenciales"]; //Se puede ser mas especifico, decir que en credenciales se va a mandar un objeto.
         }
     }
 }
--- Login_Taller_Babel/Test/LoginTest.cs
+++ Login_Taller_Babel/Test/LoginTest.cs
@@ -2,6 +2,7 @@ using Login_Taller_Babel.Genericos;
 using Login_Taller_Babel.PageObject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections;
 
 namespace Login_Taller_Babel.Test
 {
@@ -23,7 +24,7 @@ namespace Login_Taller_Babel.Test
 
             login = new LoginPage(driver); //El driver definido en el test va a viajar a LoginPage
 
-            json = new LeerJson();
+            //json = new LeerJson();
         }
 
         [TearDown]
@@ -33,12 +34,24 @@ namespace Login_Taller_Babel.Test
             driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
         }
 
-        [Test] 
-        public void IngresoCorrecto()
+        //El tipo de retorno va a ser una interfaz que va a representar una colección de objetos la que esta en credenciales.
+        //Lo que va hacer NUnit va a tomar este objeto de tipo IEnumerable y lo va a transformar en una lista de datos.
+        public static IEnumerable TestData
+        {
+            get
+            {
+                var json = new LeerJson();
+                return json.login_data().Select(data => new TestCaseData(data.username, data.password));
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(TestData))]
+        public void IngresoCorrecto(String user, String pass)
         {
             var data = json.login_data();
-            String user = data.username;
-            String pass = data.password;
+            //String user = data.username;
+            //String pass = data.password;
 
             login.IngresarCredenciales(user, pass);
         }
