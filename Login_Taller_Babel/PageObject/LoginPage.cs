8ae056e0e68b8198ca98998d4e13c5eb5534899b
Luis <luisgerardobarqueroaguilar@gmail.com>
2024-11-25 18:19:34 -0600

    Se agrego modelo POM

---
+++ Login_Taller_Babel/PageObject/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Taller_Babel.PageObject
+{
+    public class LoginPage
+    {
+        public IWebDriver _driver; // _ significa que esta variable es privada solo de esta clase, nomenclatura de C#
+
+        public LoginPage(IWebDriver driver) //Esta pagina que es la misma va a recibir un driver de alguien más
+        {
+            _driver = driver;
+        }
+
+        //Privados para que solo esta clase tenga acceso a estos localizadores. Solo lectura porque la idea es que el valor de esos elementos no cambie durante el test
+        private readonly By _txtUserName = By.Id("username");
+        private readonly By _txtPassword = By.Id("password");
+        private readonly By _btnLogin = By.CssSelector("#login > button");
+
+        public IWebElement username => _driver.FindElement(_txtUserName);
+        public IWebElement password => _driver.FindElement(_txtPassword);
+        public IWebElement botonLogin => _driver.FindElement(_btnLogin);
+
+        public void IngresarCredenciales()
+        {
+            username.SendKeys("tomsmith");
+            password.SendKeys("SuperSecretPassword!");
+            botonLogin.Click();
+        }
+
+    }
+}
--- Login_Taller_Babel/Test/LoginTest.cs
+++ Login_Taller_Babel/Test/LoginTest.cs
@@ -1,7 +1,8 @@
+using Login_Taller_Babel.PageObject;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace Login_Taller_Babel
+namespace Login_Taller_Babel.Test
 {
     public class Tests
     {
@@ -13,9 +14,9 @@ namespace Login_Taller_Babel
             driver.Manage().Window.Maximize();//Buena practica es maximizar el navegador
             driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
 
-            driver.FindElement(By.Id("username")).SendKeys("tomsmith");
-            driver.FindElement(By.Id("password")).SendKeys("SuperSecretPassword!");
-            driver.FindElement(By.CssSelector("#login > button")).Click();
+            LoginPage login = new LoginPage(driver); //El driver definido en el test va a viajar a LoginPage
+
+            login.IngresarCredenciales();
 
             driver.Close();//Cerrar el navegador
             driver.Quit();//Cerrar el ChromeDriver del administrador de tareas
